7f58cc305b0491c3715c39a2c2704e9e2fb00f6b
Ultik <104308255+ImUltik@users.noreply.github.com>
2025-11-15 01:14:08 +0100

    Fix case of 'Chill' in administratorRoles

--- sirius.lua
+++ sirius.lua
@@ -110,7 +110,7 @@ local siriusValues = {
 		adjectives = {"Cool", "Awesome", "Epic", "Ninja", "Super", "Mystic", "Swift", "Golden", "Diamond", "Silver", "Mint", "Roblox", "Amazing"},
 		nouns = {"Player", "Gamer", "Master", "Legend", "Hero", "Ninja", "Wizard", "Champion", "Warrior", "Sorcerer"}
 	},
-	administratorRoles = {"mod","admin","staff","dev","founder","owner","supervis","manager","management","executive","president","chairman","chairwoman","chairperson","Chill"},
+	administratorRoles = {"mod","admin","staff","dev","founder","owner","supervis","manager","management","executive","president","chairman","chairwoman","chairperson","chill"},
 	-- Admin User IDs and their tags
 	adminUsers = {
 		[1028438724] = "Owner",
